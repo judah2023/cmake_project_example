c75be240ec2c9d9d92668506a1f3a0e28773083f
GeonHo Noh <judah2013@naver.com>
2023-12-17 00:36:21 +0900

    Make First Window

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,39 @@
-#include <iostream>
+#include <spdlog/spdlog.h>
+#include <GLFW/glfw3.h>
 
-using namespace std;
-
-int main()
+int main(int argc, const char** argv)
 {
-    cout << "Hello, World!" << endl;
+    // 시작을 알리는 로그
+    SPDLOG_INFO("Start Program!");
+
+    // glfw 라이브러리 초기화, 실패하면 에러 출력후 종료
+    SPDLOG_INFO("Initialize GLFW");
+    if (!glfwInit()) 
+    {
+        const char* description = nullptr;
+        glfwGetError(&description);
+        SPDLOG_ERROR("failed to initialize glfw: {}", description);
+        return -1;
+    }
+
+    // glfw 윈도우 생성, 실패하면 에러 출력후 종료
+    // glfw 윈도우 생성, 실패하면 에러 출력후 종료
+    SPDLOG_INFO("Create glfw window");
+    auto window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME, nullptr, nullptr);
+    if (!window) 
+    {
+        SPDLOG_ERROR("failed to create glfw window");
+        glfwTerminate();
+        return -1;
+    }
+
+    // glfw 루프 실행, 윈도우 close 버튼을 누르면 정상 종료
+    SPDLOG_INFO("Start main loop");
+    while (!glfwWindowShouldClose(window)) 
+    {
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
